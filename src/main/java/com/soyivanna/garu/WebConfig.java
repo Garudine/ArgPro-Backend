6b68bf7979a8bbd4c6835b1a5f7e7dfb2c567404
Ivanna <117884897+Garudine@users.noreply.github.com>
2023-05-18 18:44:01 -0300

    Arreglo de CORS

---
+++ src/main/java/com/soyivanna/garu/WebConfig.java
@@ -0,0 +1,22 @@
+package com.soyivanna.garu;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * Clase que habilita CORS
+ *
+ */
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
